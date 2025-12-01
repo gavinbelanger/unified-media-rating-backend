69e044c8e0e90870d46a69eaeaa6a973cc301341
Gavin Belanger <belanger9832@uwlax.edu>
2025-12-01 11:07:18 -0600

    Initial Spring Boot project

---
+++ src/main/java/com/gavinbelanger/myumrp/MyUmrpApplication.java
@@ -0,0 +1,13 @@
+package com.gavinbelanger.myumrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUmrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyUmrpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gavinbelanger/myumrp/MyUmrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavinbelanger.myumrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUmrpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
